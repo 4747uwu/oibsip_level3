b5e93a445dcdd092b2a218229cd6b080caf59716
Abhishek Shukla <4747uwu@gmail.com>
2025-03-14 17:43:46 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://4747uwu:fitkymA4NsAHdNoM@cluster0.lr1ko.mongodb.net/pizzas?retryWrites=true&w=majority');
+    await mongoose.connect('');
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
